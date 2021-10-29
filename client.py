57155cb42dac3ea00d292f58bd52e0917c03a824
unknown <truongvanhao159@gmail.com>
2021-10-29 14:06:25 +0700

    first

---
+++ client.py
@@ -0,0 +1,328 @@
+import socket
+import sys
+import tkinter as tk 
+from tkinter import messagebox
+from tkinter import ttk 
+from tkinter import *
+import threading
+from types import NoneType
+import time
+
+
+HOST = "127.0.0.1"
+PORT = 65432
+FORMAT = "utf-8"
+DISCONNECT = "x"
+
+#option 
+SIGNUP = "signup"
+LOGIN = "login"
+LOGOUT = "logout"
+
+SUCCESS = "success"
+FAIL = "fail"
+
+CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_addr = (HOST, PORT)
+CLIENT.connect(server_addr)
+
+def sendList(CLIENT, list):
+    
+    for item in list:
+        CLIENT.sendall(item.encode(FORMAT))
+        #wait response
+        CLIENT.recv(1024)
+        
+    msg = "end"
+    CLIENT.send(msg.encode(FORMAT))
+    
+def clientLogin(CLIENT):
+    
+    username = input('Username: ')
+    password = input('Password: ')
+    account = []
+    #check account
+    account.append(username)
+    account.append(password)
+    #send Account to server to check
+    sendList(CLIENT, account)
+    
+
+
+
+
+#GUI DESIGN APP
+class currencyExchangeRate_VietNam_App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        
+        self.title("Client App")
+        self.iconbitmap('Images\money.ico')
+        self.geometry("500x300")
+        self.resizable(100, 100)
+        self.protocol("WM_DELETE_WINDOW", self.closeApp)
+        
+        container = tk.Frame()
+        container.configure(bg="red")
+        
+        container.pack(side = "top", fill = "both", expand=True)        
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (startPage, homePage, signupPage, adminPage):
+            frame = F(container, self)
+            frame.grid(row = 0, column = 0, sticky="nsew")
+            self.frames[F] = frame
+        self.frames[startPage].tkraise()
+    def showPage(self, FrameClass):
+        self.frames[FrameClass].tkraise()
+    
+    def showFrame(self, container):
+        frame = self.frames[container]
+        if container==homePage:
+            self.geometry("700x500")
+        elif container == adminPage:
+            self.geometry("450x500")
+        else:
+            self.geometry("500x200")
+        frame.tkraise()
+    # ask when quit
+    def closeApp(self):
+        if messagebox.askokcancel("Quit", "You really want to quit this usefull app ?"):
+            self.destroy()
+            try: 
+                option = LOGOUT;
+                CLIENT.sendall(option.encode(FORMAT))
+            except:
+                pass
+    # login
+    def loginApp(self, curFrame, sck):
+        try: 
+            username = curFrame.entry_user.get()
+            password = curFrame.entry_pass.get()
+            if username == "" or password == "":
+                curFrame.label_notice = "Fields cannot be empty"
+                return 
+            
+            # Notice to server for starting log in
+            option = LOGIN
+            sck.sendall(option.encode(FORMAT))
+            
+            #send username and password to server 
+            sck.sendall(username.encode(FORMAT))
+            print("Input: ", username)
+            
+            sck.recv(1024)
+            print("Server responded")                  
+            
+            sck.sendall(password.encode(FORMAT))
+            print("Input: ", password)
+            
+            sck.recv(1024)
+            print("Server responded")   
+                 
+            accepted = sck.recv(1024).decode(FORMAT)
+            print("Accepted: " + accepted)
+            
+            if accepted == "4":
+                curFrame.label_notice["text"] = "Fail to log in!"
+                if messagebox.askokcancel("Notice", "The account is active in another client!"):
+                    pass
+            elif accepted == "3":
+                if messagebox.askokcancel("Notice", "Welcome, Admin."):
+                    self.showFrame(adminPage) 
+            elif accepted == "2":
+                curFrame.label_notice["text"] = "Username or password don't correct! Please try again!"
+            elif  accepted == "1":           
+                self.showFrame(homePage)
+                
+            
+                
+            
+            
+        except:
+            curFrame.label_notice["text"] = "Error: Server is not responding"
+            print("Error: Server is not responding")
+    
+    # sign up
+    def signupApp(self, curFrame, sck):
+        try: 
+            username = curFrame.entry_user.get()
+            password = curFrame.entry_pass.get()
+            passwordAga = curFrame.entry_passAgain.get()
+            
+            if(password != passwordAga):
+                curFrame.label_notice["text"] = "Password again don't correct!"
+                return
+            
+            
+            option = SIGNUP 
+            sck.sendall(option.encode(FORMAT))                                   
+            
+            sck.sendall(username.encode(FORMAT))
+            print("Input: ", username)
+            
+            sck.recv(1024)
+            print("Server responded") 
+            
+            sck.sendall(password.encode(FORMAT))
+            print("Input: ", password)
+            
+            sck.recv(1024)
+            print("Server responded")
+            
+            accepted = sck.recv(1024).decode(FORMAT)
+            print("Accepted: " + accepted)
+            
+            if accepted == "True":
+                self.showFrame(startPage)
+                curFrame.label_notice["text"] = ""
+            else:
+                curFrame.label_notice["text"] = "Username already exists. Please using other username!"
+               
+        except:
+            curFrame.label_notice["text"] = "Error: Server is not responding"
+            print("Error: Server is not responding")
+            
+    # logout account 
+    def logoutApp(self,curFrame, sck):
+        try:
+            option = LOGOUT
+            sck.sendall(option.encode(FORMAT))
+            accepted = sck.recv(1024).decode(FORMAT)
+            if accepted == "True":
+                self.showFrame(startPage)
+        except:
+            curFrame.label_notice["text"] = "Error: Server is not responding"
+            print("Error: Server is not responding")
+            
+        
+class startPage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        
+        
+        
+          
+        label_title = tk.Label(self, text="LOGIN", fg='#20639b',bg="bisque2",font=("Times New Roman", 16, "bold"))
+        
+        label_pass = tk.Label(self, text="Password ",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        
+        self.label_notice = tk.Label(self,text="",fg='red',bg="bisque2")
+        
+        label_user = tk.Label(self, text="Username ",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        self.entry_user = tk.Entry(self,width=30,bg='light yellow')
+        self.entry_pass = tk.Entry(self,width=30,show="*",bg='light yellow')
+        
+        button_log = tk.Button(self,text="LOG IN", bg="#20639b",fg='floral white', command = lambda: app_controller.loginApp(self, CLIENT)) 
+        button_log.configure(width=20)
+        button_sign = tk.Button(self,text="SIGN UP",bg="#20639b",fg='floral white', command = lambda: app_controller.showPage(signupPage)) 
+        button_sign.configure(width=20)
+        
+        
+        label_title.pack(pady=5)
+        label_user.pack()
+        self.entry_user.pack()
+        label_pass.pack()
+        self.entry_pass.pack()
+        self.label_notice.pack()
+
+        button_log.pack(pady=5)
+        button_sign.pack()
+        
+        
+class signupPage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        
+        label_title = tk.Label(self, text="SIGNUP", fg='#20639b',bg="bisque2", font=("Times New Roman", 16, "bold"))
+        label_user = tk.Label(self, text="Username",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        label_pass = tk.Label(self, text="Password",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        label_passAgain = tk.Label(self, text="Password again",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        
+        self.label_notice = tk.Label(self,text="",bg="bisque2")
+        self.entry_user = tk.Entry(self,width=30,bg='light yellow')
+        self.entry_pass = tk.Entry(self,width=30,show="*",bg='light yellow')
+        self.entry_passAgain = tk.Entry(self,width=30,show="*",bg='light yellow')
+        
+        button_log = tk.Button(self,text="RETURN LOG IN", bg="#20639b",fg='floral white', command = lambda: app_controller.showPage(startPage)) 
+        button_log.configure(width=20)
+        button_sign = tk.Button(self,text="SIGN UP",bg="#20639b",fg='floral white',  command = lambda: app_controller.signupApp(self, CLIENT)) 
+        button_sign.configure(width=20)
+        
+        
+        label_title.pack(pady=5)
+        label_user.pack()
+        self.entry_user.pack()
+        label_pass.pack()
+        self.entry_pass.pack()
+        label_passAgain.pack()
+        self.entry_passAgain.pack()
+        self.label_notice.pack()
+
+        button_log.pack(pady=5)
+        button_sign.pack()
+class adminPage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        
+        
+class homePage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        label_title = tk.Label(self, text="HOMEPAGE", fg='#20639b',bg="bisque2")
+        label_title.pack()
+        # tra cuu thong tin
+
+
+app = currencyExchangeRate_VietNam_App()
+
+
+
+
+try: 
+    app.mainloop()
+except:
+    print("ERROR! CLIENT CAN'T CONNECT TO SERVER")
+    CLIENT.close()
+finally:
+    CLIENT.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CLIENT.close();
---
+++ server.py
@@ -0,0 +1,323 @@
+import socket
+import tkinter as tk 
+from tkinter import messagebox
+from tkinter import ttk 
+from tkinter import *
+import threading
+
+import pyodbc
+from tkinter import Entry, Tk
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+
+HOST = "127.0.0.1"
+PORT = 65432
+FORMAT = "utf-8"
+DISCONNECT = "x"
+
+#option 
+SIGNUP = "signup"
+LOGIN = "login"
+LOGOUT = "logout"
+
+SUCCESS = "success"
+FAIL = "fail"
+
+
+# server infor
+serverName = "TRUONGVANHAO\SQLEXPRESS"
+databaseAccount = "account_socket"
+databaseCurency = ""
+
+
+
+# lay du lieu tu html
+URLWEB = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx"
+def getDataFromHtml(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    data = soup.findAll("exrate")
+    listAll=[]
+
+
+    for row in data:
+        list = []
+        currencycode = row.get('currencycode')
+        currencyname = row.get('currencyname')
+        transfer = row.get('transfer')
+        buy = row.get('buy')
+        sell = row.get('sell')
+        list.append(currencyname.strip())
+        list.append(currencycode)
+        list.append(buy)
+        list.append(transfer)
+        list.append(sell)
+        listAll.append(list)
+
+    return listAll;
+    
+
+
+
+# tai khoan dang hoat dong
+liveAcc = []
+
+# kiem tra tai khoan dang hoat dong
+def checkLiveAccount(username):
+    for row in liveAcc:
+        if str(row) == username:
+            return False
+    return True
+
+
+# Tao tai khoan moi
+def createNewAccount(username, password):
+    cursor = connectToDatabase()
+    cursor.execute("insert into User_account(username,password) values(?,?);", (username, password))
+    cursor.commit()
+    
+
+
+        
+        
+# Client dang nhap vao
+def clientLogin(sck):
+    print("Log in------------------------")
+    username = sck.recv(1024).decode(FORMAT)
+    print("Username: "+ username)
+    sck.sendall(username.encode(FORMAT))
+    
+    password = sck.recv(1024).decode(FORMAT)
+    print("Password: " + password)
+    sck.sendall(password.encode(FORMAT))
+    
+    # 1. dang nhap thanh cong
+    # 2. dang nhap that bai
+    # 3. dand nhap vao admin
+    # 4. tai khoan dang hoat dong nen khong the dang nhap
+    
+    if username == "admin" and password == "123456":
+        sck.sendall("3".encode(FORMAT))
+        return
+    
+    if checkLiveAccount(username) == False:
+        sck.sendall("4".encode(FORMAT))
+        return
+    
+    try:
+        cursor = connectToDatabase()
+        cursor.execute("select password from User_account where username = ?", username)
+        check_password = cursor.fetchone()
+        data_password = check_password[0].strip()
+        if (data_password == password):
+            print("Login successfully!")
+            liveAcc.append(username)
+            sck.sendall("1".encode(FORMAT))
+        else:
+            print("Invalid password!")
+            sck.sendall("2".encode(FORMAT))
+    except:
+        print("Username is not correct!")
+        sck.sendall("2".encode(FORMAT))
+        
+# client dang ky tai khoan moi
+def clientSignup(sck):
+    print("Sign up:-------------")
+    username = sck.recv(1023).decode(FORMAT)
+    print("Username: ", username)
+    sck.sendall(username.encode(FORMAT))
+    
+    password = sck.recv(1024).decode(FORMAT)
+    print("Password: ", password)
+    sck.sendall(password.encode(FORMAT))
+    
+    # False. ton tai username
+    cursor = connectToDatabase()
+    cursor.execute("select username from User_account")
+    
+    check = True
+    if(username == "admin"):
+        sck.sendall("False".encode(FORMAT))
+        check = False
+    else:
+        # kiem tra username co ton tai hay khong
+        for row in cursor:
+            if( str(row[0]).strip() == str(username)):
+                print("sdasd")
+                sck.sendall("False".encode(FORMAT))
+                check = False
+    if check == True:
+        sck.sendall("True".encode(FORMAT))
+        createNewAccount(username, password)
+        print("Sign up successfully!")
+        
+    
+    
+    
+    
+    
+    
+            
+            
+            
+    
+    
+    
+
+# ket noi database tai khoan
+def connectToDatabase():
+    adminAcc = "admin" #tai khoan admin
+    adminPass = "123456" 
+    conx = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};\
+                            SERVER="+serverName+";\
+                            DATABASE="+databaseAccount+"; UID="+adminAcc+"; PWD="+adminPass+"")
+    cursor = conx.cursor()
+    return cursor
+
+# xu ly client da dang nhap vao
+def handleClient(conn, addr):
+    while True:
+        option = conn.recv(1024).decode(FORMAT)
+
+
+        if option == LOGIN:
+            clientLogin(conn)
+        
+        elif option == SIGNUP:
+            clientSignup(conn)
+        elif option == LOGOUT:
+            print("Client disconnected!")
+            
+    
+    
+    
+# khoi dong server
+def runServer():
+    try:
+        print(HOST, PORT)
+        print("Waiting for Client")
+        while True:
+            conn, addr = SERVER.accept()
+            print("Client: ", addr, " connected")
+            print("Conn: ", conn.getsockname())
+            
+            thr = threading.Thread(target=handleClient, args=(conn, addr))
+            thr.daemon = False
+            thr.start()
+       
+    except:
+        conn.close()
+        
+    
+    
+
+
+# GUI DESIGN APP
+class serverCurrencyExchange(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        
+        self.title("Server App")
+        self.iconbitmap('Images\money.ico')
+        self.geometry("500x300")
+        self.resizable(100, 100)
+        self.protocol("WM_DELETE_WINDOW", self.closeApp)
+        
+        container = tk.Frame()
+        container.configure(bg="red")
+        
+        container = tk.Frame()
+        container.configure(bg="red")
+        container.pack(side = "top", fill = "both", expand=True)        
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (startPage, adminPage):
+            frame = F(container, self)
+            frame.grid(row = 0, column = 0, sticky="nsew")
+            self.frames[F] = frame
+        self.frames[startPage].tkraise()
+    def showPage(self, FrameClass):
+        self.frames[FrameClass].tkraise()
+    
+    def showFrame(self, container):
+        frame = self.frames[container]
+        if container == adminPage:
+            self.geometry("450x500")
+        else:
+            self.geometry("500x200")
+        frame.tkraise()
+    # ask when quit
+    def closeApp(self):
+        if messagebox.askokcancel("Quit", "You really want to quit this usefull app ?"):
+            self.destroy()
+            SERVER.shutdown
+            
+            
+    def loginServer(self, curFrame):
+        
+        username = curFrame.entry_user.get()
+        password = curFrame.entry_pass.get()
+        
+        if password == "":
+            curFrame.label_notice["text"] = "Please typing password!"
+            
+        if username == "admin" and password == "123456":
+            self.showFrame(adminPage)
+            curFrame.label_notice["text"]=""
+        else:
+            curFrame.label_notice["text"] = "Username or password don't correct!"
+class startPage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        
+        label_title = tk.Label(self, text="\nLOGIN SERVER\n", fg='#20639b',bg="bisque2",font=("Times New Roman", 20, "bold"))
+        
+        label_pass = tk.Label(self, text="Password ",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        
+        self.label_notice = tk.Label(self,text="",fg='red',bg="bisque2")
+        
+        label_user = tk.Label(self, text="Username ",fg='#20639b',bg="bisque2",font=("Times New Roman", 12))
+        self.entry_user = tk.Entry(self,width=30,bg='light yellow')
+        self.entry_pass = tk.Entry(self,width=30,show="*",bg='light yellow')
+        
+        button_log = tk.Button(self,text="LOG IN", bg="#20639b",fg='floral white', command = lambda: app_controller.loginServer(self)) 
+        button_log.configure(width=20)
+
+        
+        
+        label_title.pack(pady=5)
+        label_user.pack()
+        self.entry_user.pack()
+        label_pass.pack()
+        self.entry_pass.pack()
+        self.label_notice.pack()
+
+        button_log.pack(pady=5)
+        
+        
+        
+class adminPage(tk.Frame):
+    def __init__(self, parent, app_controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="bisque2")
+        
+        
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind((HOST, PORT))
+SERVER.listen()
+
+# Da luong ket noi 
+sThread = threading.Thread(target=runServer)
+sThread.daemon = False
+sThread.start()
+
+app = serverCurrencyExchange()
+app.mainloop()
